663caaf2ae70745627fd58714e1abfffd7d87f3e
Mrkm6062 <akm222143@gmail.com>
2026-05-27 15:53:47 +0530

    issue is resolved

--- src/themes/theme-modern/pages/Home.jsx
+++ src/themes/theme-modern/pages/Home.jsx
@@ -7,7 +7,7 @@ import StoreLayout from '../Layout';
 import Banner from '../components/Banner';
 import ProductGrid from '../components/ProductGrid';
 import CategoryCard from '../components/CategoryCard';
-import Story from '../components/story';
+import Story from '../components/Story';
 
 const StoreHome = () => {
   const { store, loading: storeLoading, error: storeError } = useStore();
