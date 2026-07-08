6576571c209588bc4c2a9ba5afb23fa56bd79a03
Mrkm6062 <akm222143@gmail.com>
2026-07-08 12:02:35 +0530

    ui is updated of location top bar

--- src/themes/theme-localshop/pages/Home.jsx
+++ src/themes/theme-localshop/pages/Home.jsx
@@ -333,12 +333,9 @@ const StoreHome = () => {
             <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3 text-left">
               <button 
                 onClick={() => setShowAddressModal(true)}
-                className="w-full sm:w-auto px-4 py-2 bg-[#76b900] text-white text-xs font-bold rounded-xl shadow-md shadow-green-100 hover:opacity-95 transition shrink-0 text-center animate-pulse"
+                className="w-full sm:w-auto px-4 py-2 bg-[#76b900] text-white text-xs font-bold rounded-xl shadow-md shadow-green-100 hover:opacity-95 transition shrink-0 text-center"
               >
-                <MapPin className="text-gray-400" size={18} />
-                <span className="text-xs sm:text-sm text-slate-600 font-semibold">
-                  check delivery availability
-                </span>
+                <MapPin className="text-black" size={18} /> check delivery availability
               </button>
             </div>
           )}
